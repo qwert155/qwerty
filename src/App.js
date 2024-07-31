f51474b9dddc22d7b05f5e7ed4c36f2d84d1c46e
qwerty12 <ramazon11t@gmail.com>
2024-07-31 21:03:00 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 
+// import React, { useState } from 'react';
 function App() {
+
+  // const [inp1, setInp1] = useState('');
+  // const [inp2, setInp2] = useState('');
+  // const setData = () => {
+  //   const output1 = inp1 && inp2 ? alert("send") : alert("none");
+  // };
+    const handleClick = () => {
+      window.location.href = "tel:+12345678";
+    };
   return (
-    <div className="App">
-      
+    <div className="app">
+      {/* <input type="text" value={inp1} onChange={(e) => setInp1(e.target.value)} />
+      <input type="text" value={inp2} onChange={(e) => setInp2(e.target.value)} />
+      <button onClick={setData}>click</button> */}
+
+        <button onClick={handleClick}>Click</button>
     </div>
   );
 }
