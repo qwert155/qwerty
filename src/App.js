2f98928e49773defae26830eb43744888a2004c8
qwerty12 <ramazon11t@gmail.com>
2024-08-01 10:36:47 +0500

    lesson

--- src/App.js
+++ src/App.js
@@ -1,301 +1,51 @@
 import './App.css';
-import Carousel from 'react-bootstrap/Carousel'
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import React, { useState } from 'react';
+
 function App() {
 
-  // const [inp1, setInp1] = useState('');
-  // const [inp2, setInp2] = useState('');
-  // const setData = () => {
-  //   const output1 = inp1 && inp2 ? alert("send") : alert("none");
-  // };
-
-  return (
-    <div className="app">
-      {/* <input type="text" value={inp1} onChange={(e) => setInp1(e.target.value)} />
-      <input type="text" value={inp2} onChange={(e) => setInp2(e.target.value)} />
-      <button onClick={setData}>click</button> */}
-
-<div  className="teachers">
-        <div className="text_teachers">
-            <h1>У нас <span className='span_teach'>крутые  учителя</span></h1>
-        </div>
-        <div className="box_teachers">
-        <div className="first_teach">
-        <div className="expirions">
-            <h1><span>Имя</span>: Nico</h1>
-            <h1><span>Опыт Работы</span>: 6</h1>
-            <h1><span>Возраст</span>: 26</h1>
-            <h1><span>Компанни которые они работали</span>: Saudi Aramco,Tesla</h1>
-          </div>
-          <div className="hight_teach">
-            <div className="img_back">
-              <img src="https://static.vecteezy.com/system/resources/previews/025/003/295/original/3d-cute-cartoon-male-teacher-character-on-transparent-background-generative-ai-png.png" alt="" />
-            </div>
-          </div>
-          <div className="down_teach">
-              <h3>Mr.BlaBlaBla</h3>
-          </div>
-        </div>
-        <div className="first_teach ">
-        <div className="expirions">
-            <h1><span>Имя</span>: Magnus</h1>
-            <h1><span>Опыт Работы</span>: 3</h1>
-            <h1><span>Возраст</span>: 28</h1>
-            <h1><span>Компанни которые они работали</span>: NVIDIA</h1>
-          </div>
-          <div className="hight_teach">
-            <div className="img_back">
-            <img src="https://static.vecteezy.com/system/resources/previews/025/003/295/original/3d-cute-cartoon-male-teacher-character-on-transparent-background-generative-ai-png.png" alt="" />
-            </div>
-          </div>
-          <div className="down_teach">
-              <h3>Mr.BlaBlaBla</h3>
-          </div>
-        </div>
-        <div className="first_teach">
-        <div className="expirions">
-            <h1><span>Имя</span>: Felix</h1>
-            <h1><span>Опыт Работы</span>: 6</h1>
-            <h1><span>Возраст</span>: 20</h1>
-            <h1><span>Компанни которые они работали</span>: Microsoft,Amazon</h1>
-          </div>
-          <div className="hight_teach">
-            <div className="img_back">
-            <img src="https://static.vecteezy.com/system/resources/previews/025/003/295/original/3d-cute-cartoon-male-teacher-character-on-transparent-background-generative-ai-png.png" alt="" />
-            </div>
-          </div>
-          <div className="down_teach">
-              <h3>Mr.BlaBlaBla</h3>
-          </div>
-        </div>
-        <div className="first_teach">
-        <div className="expirions">
-            <h1><span>Имя</span>: Carlo</h1>
-            <h1><span>Опыт Работы</span>: 10 лет</h1>
-            <h1><span>Возраст</span>: 34</h1>
-            <h1><span>Компанни которые они работали</span>:Google,Audi,Inter</h1>
-          </div>
-          <div className="hight_teach">
-            <div className="img_back">
-            <img src="https://static.vecteezy.com/system/resources/previews/025/003/295/original/3d-cute-cartoon-male-teacher-character-on-transparent-background-generative-ai-png.png" alt="" />
-            </div>
-          </div>
-          <div className="down_teach">
-              <h3>Mr.BlaBlaBla</h3>
-          </div>
-        </div>
-        <div className="first_teach">
-        <div className="expirions">
-            <h1><span>Имя</span>: Greg</h1>
-            <h1><span>Опыт Работы</span>: 5 лет</h1>
-            <h1><span>Возраст</span>: 25</h1>
-            <h1><span>Компанни которые они работали</span>: Yandex,Google,React</h1>
-          </div>
-          <div className="hight_teach">
-            <div className="img_back">
-            <img src="https://static.vecteezy.com/system/resources/previews/025/003/295/original/3d-cute-cartoon-male-teacher-character-on-transparent-background-generative-ai-png.png" alt="" />
-            </div>
-          </div>
-          <div className="down_teach">
-              <h3>Mr.BlaBlaBla</h3>
-          </div>
-        </div>
-        </div>
-</div>
-      <div className="h1_carusel">
-          <h1>У нас <span className='span_teach_caruusel'>крутые  учителя</span></h1>
+return (
+<div className="app">
+      <div className="lesson">
+      <div className="txt_lesson">
+        <h1>Наши <span>курсы</span></h1>
       </div>
-      <Carousel data-bs-theme="dark" className='carusel_teachers'>
-      {/* 1 */}
-      <Carousel.Item className='center_flex'>
-      <div className="flex_box">
-      <div className="first_teach">
-        <div className="expirions">
-            <h1><span>Имя</span>: Greg</h1>
-            <h1><span>Опыт Работы</span>: 5 лет</h1>
-            <h1><span>Возраст</span>: 25</h1>
-            <h1><span>Компанни которые они работали</span>: Yandex,Google,React</h1>
-          </div>
-          <div className="hight_teach">
-            <div className="img_back">
-            <img src="https://static.vecteezy.com/system/resources/previews/025/003/295/original/3d-cute-cartoon-male-teacher-character-on-transparent-background-generative-ai-png.png" alt="" />
-            </div>
-          </div>
-          <div className="down_teach">
-              <h3>Mr.BlaBlaBla1</h3>
-          </div>
-        </div>
-        <div className="first_teach big_flex">
-        <div className="expirions">
-            <h1><span>Имя</span>: Greg</h1>
-            <h1><span>Опыт Работы</span>: 5 лет</h1>
-            <h1><span>Возраст</span>: 25</h1>
-            <h1><span>Компанни которые они работали</span>: Yandex,Google,React</h1>
-          </div>
-          <div className="hight_teach">
-            <div className="img_back">
-            <img src="https://static.vecteezy.com/system/resources/previews/025/003/295/original/3d-cute-cartoon-male-teacher-character-on-transparent-background-generative-ai-png.png" alt="" />
-            </div>
-          </div>
-          <div className="down_teach">
-              <h3>Mr.BlaBlaBla2</h3>
-          </div>
-        </div>
+      <div className="box_lesson">
+              <div className="first_box_lesson">
+                <img src="https://static.tildacdn.one/tild3335-6231-4765-b331-653232316637/startup_company_rock.svg" alt="" />
+                <h2>Foundation</h2>
+                <hr />
+                <h2 className='h2_box'>Продолжительность : 3 месяца</h2>
+                <hr />
+                <h2>Время : 1,5 часа</h2>
+                <hr />
+                <h2>Дни : 3 дня в неделю</h2>
+                <hr />
+              </div>
+              <div className="first_box_lesson">
+                <img src="	https://static.tildacdn.one/tild3132-3838-4965-b738-356239383834/research_eye_browser.svg" alt="" />
+                <h2>Frontend</h2>
+                <hr />
+                <h2 className='h2_box'>Продолжительность: 8 месяцев</h2>
+                <hr />
+                <h2>Время : 1,5 часа</h2>
+                <hr />
+                <h2>Дни : 3 дня в неделю</h2>
+                <hr />
+              </div>
+              <div className="first_box_lesson">
+                <img src="https://static.tildacdn.one/tild3336-6333-4931-b638-346365663434/tech_tuning_support.svg" alt="" />
+                <h2>Backend</h2>
+                <hr />
+                <h2 className='h2_box'>Продолжительность: 8 месяцов</h2>
+                <hr />
+                <h2>Время : 1,5 часа</h2>
+                <hr />
+                <h2>Дни : 3 дня в неделю</h2>
+                <hr />
+              </div>
       </div>
-      </Carousel.Item>
-      {/* 2 */}
-      <Carousel.Item className='center_flex'>
-      <div className="flex_box">
-      <div className="first_teach">
-        <div className="expirions">
-            <h1><span>Имя</span>: Greg</h1>
-            <h1><span>Опыт Работы</span>: 5 лет</h1>
-            <h1><span>Возраст</span>: 25</h1>
-            <h1><span>Компанни которые они работали</span>: Yandex,Google,React</h1>
-          </div>
-          <div className="hight_teach">
-            <div className="img_back">
-            <img src="https://static.vecteezy.com/system/resources/previews/025/003/295/original/3d-cute-cartoon-male-teacher-character-on-transparent-background-generative-ai-png.png" alt="" />
-            </div>
-          </div>
-          <div className="down_teach">
-              <h3>Mr.BlaBlaBla3</h3>
-          </div>
-        </div>
-        <div className="first_teach big_flex">
-        <div className="expirions">
-            <h1><span>Имя</span>: Greg</h1>
-            <h1><span>Опыт Работы</span>: 5 лет</h1>
-            <h1><span>Возраст</span>: 25</h1>
-            <h1><span>Компанни которые они работали</span>: Yandex,Google,React</h1>
-          </div>
-          <div className="hight_teach">
-            <div className="img_back">
-            <img src="https://static.vecteezy.com/system/resources/previews/025/003/295/original/3d-cute-cartoon-male-teacher-character-on-transparent-background-generative-ai-png.png" alt="" />
-            </div>
-          </div>
-          <div className="down_teach">
-              <h3>Mr.BlaBlaBla4</h3>
-          </div>
-        </div>
-      </div>
-      </Carousel.Item>
-      {/* 3 */}
-      <Carousel.Item className='center_flex'>
-      <div className="flex_box">
-      <div className="first_teach">
-        <div className="expirions">
-            <h1><span>Имя</span>: Greg</h1>
-            <h1><span>Опыт Работы</span>: 5 лет</h1>
-            <h1><span>Возраст</span>: 25</h1>
-            <h1><span>Компанни которые они работали</span>: Yandex,Google,React</h1>
-          </div>
-          <div className="hight_teach">
-            <div className="img_back">
-            <img src="https://static.vecteezy.com/system/resources/previews/025/003/295/original/3d-cute-cartoon-male-teacher-character-on-transparent-background-generative-ai-png.png" alt="" />
-            </div>
-          </div>
-          <div className="down_teach">
-              <h3>Mr.BlaBlaBla5</h3>
-          </div>
-        </div>
-        <div className="first_teach big_flex">
-        <div className="expirions">
-            <h1><span>Имя</span>: Greg</h1>
-            <h1><span>Опыт Работы</span>: 5 лет</h1>
-            <h1><span>Возраст</span>: 25</h1>
-            <h1><span>Компанни которые они работали</span>: Yandex,Google,React</h1>
-          </div>
-          <div className="hight_teach">
-            <div className="img_back">
-            <img src="https://static.vecteezy.com/system/resources/previews/025/003/295/original/3d-cute-cartoon-male-teacher-character-on-transparent-background-generative-ai-png.png" alt="" />
-            </div>
-          </div>
-          <div className="down_teach">
-              <h3>Mr.BlaBlaBla5</h3>
-          </div>
-        </div>
-      </div>
-      </Carousel.Item>
-      {/* 4 */}
-      <Carousel.Item className='center_flex'>
-      <div className="flex_box">
-      <div className="first_teach">
-        <div className="expirions">
-            <h1><span>Имя</span>: Greg</h1>
-            <h1><span>Опыт Работы</span>: 5 лет</h1>
-            <h1><span>Возраст</span>: 25</h1>
-            <h1><span>Компанни которые они работали</span>: Yandex,Google,React</h1>
-          </div>
-          <div className="hight_teach">
-            <div className="img_back">
-            <img src="https://static.vecteezy.com/system/resources/previews/025/003/295/original/3d-cute-cartoon-male-teacher-character-on-transparent-background-generative-ai-png.png" alt="" />
-            </div>
-          </div>
-          <div className="down_teach">
-              <h3>Mr.BlaBlaBla5</h3>
-          </div>
-        </div>
-        <div className="first_teach big_flex">
-        <div className="expirions">
-            <h1><span>Имя</span>: Greg</h1>
-            <h1><span>Опыт Работы</span>: 5 лет</h1>
-            <h1><span>Возраст</span>: 25</h1>
-            <h1><span>Компанни которые они работали</span>: Yandex,Google,React</h1>
-          </div>
-          <div className="hight_teach">
-            <div className="img_back">
-            <img src="https://static.vecteezy.com/system/resources/previews/025/003/295/original/3d-cute-cartoon-male-teacher-character-on-transparent-background-generative-ai-png.png" alt="" />
-            </div>
-          </div>
-          <div className="down_teach">
-              <h3>Mr.BlaBlaBla5</h3>
-          </div>
-        </div>
-      </div>
-      </Carousel.Item>
-      {/* 5 */}
-      <Carousel.Item className='center_flex'>
-      <div className="flex_box">
-      <div className="first_teach">
-        <div className="expirions">
-            <h1><span>Имя</span>: Greg</h1>
-            <h1><span>Опыт Работы</span>: 5 лет</h1>
-            <h1><span>Возраст</span>: 25</h1>
-            <h1><span>Компанни которые они работали</span>: Yandex,Google,React</h1>
-          </div>
-          <div className="hight_teach">
-            <div className="img_back">
-            <img src="https://static.vecteezy.com/system/resources/previews/025/003/295/original/3d-cute-cartoon-male-teacher-character-on-transparent-background-generative-ai-png.png" alt="" />
-            </div>
-          </div>
-          <div className="down_teach">
-              <h3>Mr.BlaBlaBla5</h3>
-          </div>
-        </div>
-        <div className="first_teach big_flex">
-        <div className="expirions">
-            <h1><span>Имя</span>: Greg</h1>
-            <h1><span>Опыт Работы</span>: 5 лет</h1>
-            <h1><span>Возраст</span>: 25</h1>
-            <h1><span>Компанни которые они работали</span>: Yandex,Google,React</h1>
-          </div>
-          <div className="hight_teach">
-            <div className="img_back">
-            <img src="https://static.vecteezy.com/system/resources/previews/025/003/295/original/3d-cute-cartoon-male-teacher-character-on-transparent-background-generative-ai-png.png" alt="" />
-            </div>
-          </div>
-          <div className="down_teach">
-              <h3>Mr.BlaBlaBla5</h3>
-          </div>
-        </div>
-      </div>
-      </Carousel.Item>
-
-      </Carousel>
     </div>
-  );
+</div>
+);
 }
 
 export default App;
